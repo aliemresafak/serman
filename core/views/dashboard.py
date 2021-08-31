4bf3dccf4c9340796b0848f7facf3bf86c94a900
Ali Emre Şafak <aliemresafak@gmail.com>
2021-08-31 23:00:00 +0300

    Add: not completed fixing

--- core/views/dashboard.py
+++ core/views/dashboard.py
@@ -11,8 +11,10 @@ class DashboardView(TemplateView):
 
         context["daily_repair_all"] = Service.objects.all().filter(created_at__day=datetime.now().day)
 
-        context["daily_repair_waiting"] = Service.objects.all().filter(status=Service.WAITING, created_at__day=datetime.now().day)
-        context["daily_repair_fixing"] = Service.objects.all().filter(status=Service.FIXING, created_at__day=datetime.now().day)
-        context["daily_repair_fixed"] = Service.objects.all().filter(status=Service.FIXED, created_at__day=datetime.now().day)
+        context["daily_repair_waiting"] = Service.objects.filter(status=Service.WAITING, created_at__day=datetime.now().day)
+        context["daily_repair_fixing"] = Service.objects.filter(status=Service.FIXING, created_at__day=datetime.now().day)
+        context["daily_repair_fixed"] = Service.objects.filter(status=Service.FIXED, created_at__day=datetime.now().day)
+
+        context["not_completed_fixing"] = Service.objects.filter(created_at__day__lt=datetime.now().day).exclude(status__exact=Service.FIXED)
 
         return context 
