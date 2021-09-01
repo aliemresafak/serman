b50232abdb966d9bf2b727a04ad2c6128d5f0e87
Ali Emre Şafak <aliemresafak@gmail.com>
2021-09-02 00:20:52 +0300

    Change: rename from user to customer

--- core/views/customer.py
+++ core/views/customer.py
@@ -5,23 +5,23 @@ user_fields = ["name", "surname", "telephone_number"]
 
 class UserListView(ListView):
     model = User
-    template_name = "user/users.html"
+    template_name = "customer/customer_list.html"
     context_object_name = "users"
 
 class UserCreateView(CreateView):
     model = User
-    template_name = "user/user_create.html"
+    template_name = "customer/customer_create.html"
     success_url = "/customer"
     fields = user_fields
 
 class UserDetailView(DetailView):
     model = User
-    template_name = "user/user_detail.html"
+    template_name = "customer/customer_detail.html"
     context_object_name = "customer"
     slug_field = "pk"
 
 class UserUpdateView(UpdateView):
     model = User
-    template_name = "user/user_update.html"
+    template_name = "customer/customer_update.html"
     success_url = "/customer"
     fields = user_fields
