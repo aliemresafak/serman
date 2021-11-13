68cf085359de6693266f24a536b2bd9e45721762
Ali Emre Şafak <aliemresafak@gmail.com>
2021-11-13 14:57:21 +0300

    Fix: optimize service object

--- core/views/dashboard.py
+++ core/views/dashboard.py
@@ -11,13 +11,16 @@ class DashboardView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
+        services = Service.objects.all()
+        context["daily_repair_all"] = services.filter(created_at__day=datetime.now().day)
 
-        context["daily_repair_all"] = Service.objects.all().filter(created_at__day=datetime.now().day)
+        context["daily_repair_waiting"] = services.filter(status=Service.WAITING, created_at__day=datetime.now().day)
+        context["daily_repair_fixing"] = services.filter(status=Service.FIXING, created_at__day=datetime.now().day)
+        context["daily_repair_fixed"] = services.filter(status=Service.FIXED, created_at__day=datetime.now().day)
 
-        context["daily_repair_waiting"] = Service.objects.filter(status=Service.WAITING, created_at__day=datetime.now().day)
-        context["daily_repair_fixing"] = Service.objects.filter(status=Service.FIXING, created_at__day=datetime.now().day)
-        context["daily_repair_fixed"] = Service.objects.filter(status=Service.FIXED, created_at__day=datetime.now().day)
-
-        context["not_completed_fixing"] = Service.objects.filter(created_at__day__lt=datetime.now().day).exclude(status__exact=Service.FIXED)
+        context["not_completed_fixing"] = services.filter(created_at__day__lt=datetime.now().day).exclude(status__exact=Service.FIXED)
 
         return context 
+
+class ServiceStatusView(TemplateView):
+    template_name = "service_status.html"
