b227232638f44ad3e8cfe0fa0cd79cdfc909dcb2
Ali Emre Şafak <aliemresafak@gmail.com>
2021-11-21 18:40:32 +0300

    added service status page and backend

--- core/views/dashboard.py
+++ core/views/dashboard.py
@@ -1,6 +1,7 @@
-from typing import Any, Dict
-from django.views.generic import TemplateView
 from datetime import datetime
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
 from core.models import Service
 from .base import LoginRequiredMixin
 
@@ -9,7 +10,7 @@ class DashboardView(LoginRequiredMixin, TemplateView):
     login_url = "/auth/login/"
     redirect_field_name = 'redirect_to'
 
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         services = Service.objects.all()
         context["daily_repair_all"] = services.filter(created_at__day=datetime.now().day)
@@ -24,3 +25,19 @@ class DashboardView(LoginRequiredMixin, TemplateView):
 
 class ServiceStatusView(TemplateView):
     template_name = "service_status.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+    def post(self, request):
+        service_objects = Service.objects.all()
+        service_number = request.POST.get("service_number")
+
+        data = service_objects.filter(number=service_number).first()
+
+        if data:
+            print(data)
+            return render(request, self.template_name, {"data": data, "errors": "Bu servise numarasina ait kayit bulunamadi"})
+        else:
+            print(data)
+            return render(request, self.template_name, {"data": data})
