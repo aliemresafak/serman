03fb0fab61c0583eef8bfc8cd184ff3574a16a83
Ali Emre Şafak <aliemresafak@gmail.com>
2021-11-27 12:22:42 +0300

    added remember me

--- core/views/auth.py
+++ core/views/auth.py
@@ -1,10 +1,18 @@
 from django.contrib.auth.views import LoginView, LogoutView
-
+from core.forms import AuthForm
 
 class CustomLoginView(LoginView):
     template_name = "auth/login.html"
     redirect_field_name = "home"
+    form_class = AuthForm
+    
 
+    def form_valid(self, form):
+        remember_me = form.cleaned_data["remember_me"]
+        if not remember_me:
+            self.request.session.set_expiry(0)
+            self.request.session.modified = True
+        return super(CustomLoginView, self).form_valid(form)
 
 class CustomLogoutView(LogoutView):
     next_page = "/"
